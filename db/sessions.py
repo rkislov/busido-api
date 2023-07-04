026a9425385dd1812b81a5006ec7133d5eff9e80
Roman Kislov <roman@kislovs.ru>
2023-07-05 00:48:29 +0300

    make password hashing

--- db/sessions.py
+++ db/sessions.py
@@ -1,13 +1,19 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
+from typing import Generator
 
 from core.config import settings
-
 
 SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
 print("Database URL ", SQLALCHEMY_DATABASE_URL)
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
-
 SessionLocal = sessionmaker(autocoomit=False, autoflush=False, bind=engine)
+
+
+def get_db() -> Generator:
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
