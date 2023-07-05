60e5b3e4def7b9969103cf41d2089063f536d228
Roman Kislov <roman@kislovs.ru>
2023-07-05 18:25:24 +0300

    jwt functionality

--- core/hashing.py
+++ core/hashing.py
@@ -8,7 +8,6 @@ class Hasher():
     def verify_password(plain_password, hashed_password):
         return pwd_context.verify(plain_password, hashed_password)
 
-
     @staticmethod
     def get_password_hash(password):
         return pwd_context.hash(password)
